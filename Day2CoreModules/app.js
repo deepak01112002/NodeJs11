bf54efd1a15a72099e57b4cb4d9cc7510534038b
deepak01112002 <deepak8574757609@gmail.com>
2024-10-16 12:03:07 +0530

    core modules

---
+++ Day2CoreModules/app.js
@@ -0,0 +1,58 @@
+// readfile, readfileSync
+
+const fs = require("fs")
+const os = require("os")
+const path = require("path")
+const http = require("http")
+
+
+// fs.readFile('app.txt','utf-8',(err,data)=>{
+//     if(err){
+//         console.log(err)
+//     }else{
+//         console.log("data"+data)
+//     }
+// })
+
+
+// let a = fs.readFileSync('app.txt','utf-8')
+
+// console.log(a)
+// console.log("HHHHH")
+
+// // let b = fs.readFileSync('hello.txt','utf-8')
+
+// // console.log(b)
+
+
+// writeFile, writeFileSync
+
+// fs.writeFile('app.txt',"Hello world's",(err)=>{
+//       if(err){
+//         console.log(err)
+//       }else{
+//       console.log("File written successfully")
+//       }
+// })
+
+// fs.writeFileSync("app.txt","Karan's")
+
+
+
+// OS
+
+
+// console.log(os.platform())
+
+
+// Path;
+
+
+let a = path.join(__dirname,"app.txt")
+
+console.log(path.extname(a))
+
+// http
+
+
+
