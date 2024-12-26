e0a8366f92baf7c288ca3a5b1e3c9588c5063f0a
deepak01112002 <deepak8574757609@gmail.com>
2024-12-26 11:54:52 +0530

    add

--- login_signup/app.js
+++ login_signup/app.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const connection = require("./Config/db");
 const UserRouter = require("./Routes/UserRoutes");
-const cors = require("cors")
+const cors = require("cors");
+const BlogRouter = require("./Routes/BlogRoutes");
 require("dotenv").config()
 const app = express();
 app.use(express.json());
 app.use(cors())
 
 app.use("/", UserRouter)
+app.use("/blog",BlogRouter)
+
 
 app.listen(process.env.PORT,()=>{
     connection()
